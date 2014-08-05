f5838dea665a25d37f8755fbe2af60f89cb51905
Lee Clagett <code@leeclagett.com>
2014-08-05 09:01:01 -0400

    Adding conversion functions.
    SNTP uses network order values for its primitives, some utility
    conversion functions were added.

---
+++ example/cpp11/sntp/server/conversion.hpp
@@ -0,0 +1,30 @@
+//
+// conversion.hpp
+// ~~~~~~~~~~~~~
+//
+// Copyright (c) 2014 Lee Clagett (code at leeclagett dot com)
+// 
+// Distributed under the Boost Software License, Version 1.0. (See accompanying)
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+
+#ifndef CONVERSION_HPP
+#define CONVERSION_HPP
+
+#include <boost/asio/detail/socket_ops.hpp>
+#include <cstdint>
+
+namespace sntp
+{
+    std::uint32_t to_ushort(const std::uint16_t convert)
+    {
+	return boost::asio::detail::socket_ops::host_to_network_short(convert);
+    }
+
+    std::uint32_t to_ulong(const std::uint32_t convert)
+    {
+	return boost::asio::detail::socket_ops::host_to_network_long(convert);
+    }
+}
+
+#endif // CONVERSION_HPP
