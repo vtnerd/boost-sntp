8be1044bf5f1b5f29483eb9454acaa668adac115
Lee Clagett <code@leeclagett.com>
2014-08-07 19:36:15 -0400

    Upgrading the NTP timestamp
    The NTP timestamp will now use a randomly generated value to generate a
    cryptographic image in the insignificant bits of the fractional portion
    of the timestamp. The image is based on the hashing of the random value,
    the seconds, and the significant portion of the fractional part. It can
    be used to determine whether the server instance generated a timestamp.
    
    Tests are sorely needed, as the algorithm is a little complex and easy
    to get wrong (coming shortly).

--- example/cpp11/sntp/server/timestamp.cpp
+++ example/cpp11/sntp/server/timestamp.cpp
@@ -12,9 +12,10 @@
 #include <array>
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <cmath>
+#include <complex>
 #include <cryptopp/aes.h>
 #include <cryptopp/osrng.h>
-#include <complex>
+#include <cryptopp/sha.h>
 #include <limits>
 
 #include "conversion.hpp"
@@ -23,7 +24,7 @@ namespace sntp
 {
     namespace
     {
-	// Generates a randm 128-bit value on construction
+	// Generates a random 128-bit value on construction
 	class RandomString
 	{
 	public:
@@ -35,17 +36,30 @@ namespace sntp
 		random_generator.GenerateBlock(random_.data(), random_.size());
 	    }
 
-	    const std::array<std::uint8_t, 128>& get_random_string() const
+	    const std::array<std::uint8_t, 16>& get_random_string() const
 	    {
 		return random_;
 	    }
 
 	private:
 
-	    std::array<std::uint8_t, 128> random_;
+	    std::array<std::uint8_t, 16> random_;
 	};
 
+
+
+	// random string for detecting loops and replay attacks
 	const RandomString random_data;
+
+	// masks for bits of the timestamp that (in)significant due to accuracy
+	static_assert(timestamp::precision::significant_bits() <= 64, "bad bit value");
+	const std::uint32_t insignificant_mask =
+	    to_ulong(
+		std::numeric_limits<std::uint32_t>::max() >>
+		(32 - timestamp::precision::significant_bits()));
+        const std::uint32_t significant_mask = ~insignificant_mask;
+
+	// ratio for converting microseconds to NTP fractional
 	const double fractional_ratio = std::pow(2, 32) / std::pow(10, 6);
 
 	// total_seconds returns a long, whose type is _at least_ a 32-bit
@@ -53,23 +67,19 @@ namespace sntp
 	// value if total_seconds() exceeds a 32-bit signed integer. Mitigate
 	// this by bumping the epoch to the next rollover event, but do so
 	// carefully since posix_time::second also uses type long.
-	const boost::posix_time::ptime epoch(
-	    boost::gregorian::date(1900, 1, 1),
-	    boost::posix_time::seconds(std::numeric_limits<std::int32_t>::max()) +
-	    boost::posix_time::seconds(std::numeric_limits<std::int32_t>::max()) +
-	    boost::posix_time::seconds(1));
-
-	// ensure that the the expression above == uint32 max
 	static_assert(
 	    (
 		(std::uint32_t(std::numeric_limits<std::int32_t>::max()) * 2) + 1 ==
 		std::numeric_limits<std::uint32_t>::max()),
 	    "unexpected int32::max value");
-
-	// ensure int32 max does not exceed long max
 	static_assert(
 	    std::numeric_limits<std::int32_t>::max() <= std::numeric_limits<long>::max(),
 	    "unexpected long size");
+	const boost::posix_time::ptime epoch(
+	    boost::gregorian::date(1900, 1, 1),
+	    boost::posix_time::seconds(std::numeric_limits<std::int32_t>::max()) +
+	    boost::posix_time::seconds(std::numeric_limits<std::int32_t>::max()) +
+	    boost::posix_time::seconds(1));
     }
 
     timestamp timestamp::now()
@@ -87,6 +97,40 @@ namespace sntp
 	timestamp current_time;
 	current_time.seconds_ = to_ulong(seconds_since_epoch);
 	current_time.fractional_ = to_ulong(std::uint32_t(microsecond_precision * fractional_ratio));
+        current_time.generate_crypto_string();
 	return current_time;
+    }
+
+    bool timestamp::from_server() const
+    {
+        timestamp crypto(*this);
+        crypto.generate_crypto_string();
+	return crypto.seconds_ == seconds_ && crypto.fractional_ == fractional_;
+    }
+
+    void timestamp::generate_crypto_string()
+    {
+	std::uint32_t crypto_string = 0;
+        fractional_ &= significant_mask;
+	{
+	    std::array<std::uint8_t, CryptoPP::SHA256::DIGESTSIZE> hash_value;
+
+            CryptoPP::SHA256 hash;
+	    hash.Update(
+		random_data.get_random_string().data(),
+		random_data.get_random_string().size());
+            hash.Update(
+                reinterpret_cast<const std::uint8_t*>(&seconds_),
+                sizeof(seconds_));
+	    hash.Update(
+		reinterpret_cast<const std::uint8_t*>(&fractional_),
+		sizeof(fractional_));
+	    hash.Final(hash_value.data());
+
+	    static_assert(sizeof(crypto_string) <= hash_value.size(), "hash is too small");
+	    std::memcpy(&crypto_string, hash_value.data(), sizeof(crypto_string));
+	}
+	crypto_string &= insignificant_mask;
+	fractional_ |= crypto_string;
     }
 }
