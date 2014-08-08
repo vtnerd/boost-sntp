2a617ee928c944b9a73e11f83ee87fb974a8473f
Lee Clagett <code@leeclagett.com>
2014-08-07 22:29:52 -0400

    Properly setting flags in response packets.

--- example/cpp11/sntp/server/packet.cpp
+++ example/cpp11/sntp/server/packet.cpp
@@ -16,9 +16,9 @@ namespace sntp
 {
     namespace
     {
-        void set_flags(std::uint8_t& flags)
-        {
-        }
+        const std::uint8_t alarm_condition = 0xC0;
+        const std::uint8_t version = 0x20;
+        const std::uint8_t server = 0x04;
 
         const std::uint8_t primary_reference = 1;
         const std::uint8_t sixty_four_second_poll_interval = 6;
@@ -48,7 +48,7 @@ namespace sntp
         {
             receive_ = timestamp::now();
 
-            set_flags(flags_);
+            flags_ = alarm_condition | version | server;
             stratum_ = primary_reference;
             poll_ = sixty_four_second_poll_interval;
             precision_ = timestamp::precision();
@@ -62,7 +62,6 @@ namespace sntp
             originate_ = transmit_;
 
             transmit_ = timestamp::now();
-
             return true;
         }
 
