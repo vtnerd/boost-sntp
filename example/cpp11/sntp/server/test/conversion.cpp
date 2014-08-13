0dfe63fefb7d685279091105b29ddeddcbe81cff
Lee Clagett <code@leeclagett.com>
2014-08-13 10:17:40 -0400

    Adding some tests for timestamp, could use a few more

--- example/cpp11/sntp/server/test/conversion.cpp
+++ example/cpp11/sntp/server/test/conversion.cpp
@@ -6,6 +6,6 @@ int test_main(int, char**)
 {
     BOOST_CHECK(sntp::to_ulong(0xDEADBEEF) == sntp::to_ulong(0xDEADBEEF));
     // let fail on big-endian for now
-    BOOST_CHECK(sntp::to_ulong(0xDEADBEEF) == 0xEFBEADDE); 
+    BOOST_CHECK(sntp::to_ulong(0xDEADBEEF) == 0xEFBEADDE);
     return 0;
 }
---
+++ example/cpp11/sntp/server/test/timestamp.cpp
@@ -0,0 +1,77 @@
+#include <boost/range/algorithm/count.hpp>
+#include <boost/range/iterator_range.hpp>
+#include <boost/test/minimal.hpp>
+#include <cstdint>
+
+#include "conversion.hpp"
+#include "timestamp.hpp"
+
+namespace 
+{
+    auto make_range(const sntp::timestamp& time)
+    {
+	return boost::make_iterator_range(
+	    reinterpret_cast<const std::uint8_t*>(&time),
+	    reinterpret_cast<const std::uint8_t*>(&time) + sizeof(time));
+    }
+
+    auto get_values(const sntp::timestamp& time)
+    {
+	BOOST_REQUIRE(sizeof(time) == sizeof(std::uint32_t) * 2);
+
+	const auto range = make_range(time);
+	std::uint32_t seconds = 0;
+	std::uint32_t fractional = 0;
+
+	std::memcpy(&seconds, range.begin(), sizeof(seconds));
+	std::memcpy(
+	    &fractional, range.begin() + sizeof(seconds), sizeof(fractional));
+
+	return std::make_pair(seconds, fractional);
+    }
+
+    auto make_timestamp(const std::pair<std::uint32_t, std::uint32_t>& values)
+    {
+	sntp::timestamp time;
+	std::memcpy(&time, &values.first, sizeof(values.first));
+	std::memcpy(
+	    reinterpret_cast<boost::uint8_t*>(&time) + sizeof(values.first), 
+	    &values.second, 
+	    sizeof(values.second));
+	return time;
+    }
+}
+
+int test_main(int, char**)
+{
+    {
+	const sntp::timestamp::precision precise;
+	BOOST_CHECK(sntp::timestamp::precision::significant_bits() == 20);
+	BOOST_CHECK(sizeof(precise) == sizeof(std::int8_t));
+	BOOST_CHECK(
+	    *reinterpret_cast<const std::int8_t*>(&precise) == -20);
+    }
+    {
+	const sntp::timestamp time;
+	BOOST_CHECK(!time.from_server());
+	BOOST_CHECK(
+	    boost::range::count(make_range(time), 0) == sizeof(time));
+    }
+    {
+	const sntp::timestamp time = sntp::timestamp::now();
+	BOOST_CHECK(time.from_server());
+
+	auto values = get_values(time);
+	values.second = ~(values.first & sntp::to_ulong(0x01));
+
+	const sntp::timestamp modified_time = make_timestamp(values);
+	const auto modified_values = get_values(modified_time);
+
+	BOOST_CHECK(!modified_time.from_server());
+	BOOST_CHECK(modified_values.first == values.first);
+	BOOST_CHECK((modified_values.second & sntp::to_ulong(0xFFFFFFFE)) == 
+		    (values.second & sntp::to_ulong(0xFFFFFFFE)));
+    }
+
+    return 0;
+}
